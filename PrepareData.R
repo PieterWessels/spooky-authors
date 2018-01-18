f2085752e3a985b8a99a94a93ec809bed2242f7f
PIETER WESSELS <pieter.wessels1@dwp.gsi.gov.uk>
2018-01-18 07:35:57 +0000

    feat:Remove Stopwords

--- PrepareData.R
+++ PrepareData.R
@@ -13,9 +13,10 @@ training_data <- read.csv(file=training_data_filename, stringsAsFactors=FALSE)
 #  remove stop words  (before or after stemming?)
 #  calculate token frequencies
 
-
+library("tm")
 # Clean a single "document"
-document <- "This process, however, afforded me no means of ascertaining the dimensions of my dungeon; as I might make its circuit, and return to the point whence I set out, without being aware of the fact; so perfectly uniform seemed the wall."
+# example below contains 3 x process
+document <- "process. This process, however, afforded me no means of ascertaining process the dimensions of my dungeon; as I might make its circuit, and return to the point whence I set out, without being aware of the fact; so perfectly uniform seemed the wall."
 
 document <- tolower(document)
 document <- gsub(pattern="[[:punct:]]",
@@ -28,4 +29,15 @@ document <- gsub(pattern="[[:space:]]+",
                  x=document,
                  replacement=" ")
 
-#Tokenising & (Stemming or Lemitization)
+#Convert to vector
+data <- unlist(strsplit(x=document, split=" "))
+
+#Remove Stopwords
+#Apparently tm contains a list of industry standard english stopwords
+stopword_index <- which(data %in% tm::stopwords(kind="en"))
+data <- data[-stopword_index]
+
+#Stem
+#For ease of use, use the Porter stemming algorithm in tm
+
+
